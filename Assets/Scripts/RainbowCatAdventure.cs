5216c28e2d0895db4cd61169102cf4662cc16cfd
nick-gharizadeh <nick-gharizadeh@gmail.com>
2024-06-04 14:45:40 +0330

    fea / show random cats on scene + show random color on scene

--- Assets/Scripts/RainbowCatAdventure.cs
+++ Assets/Scripts/RainbowCatAdventure.cs
@@ -1,42 +1,113 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class RainbowCatAdventure : MonoBehaviour
 {
+    List<string> colors= new List<string> { "black","blue","brown","gray","green","orange","pink","purple","red","yellow" };
+    List<string> randomColors = new List<string>();
+    Hashtable directionWithCats = new Hashtable();
+    string rightAnswerColor , rightAnswerDirection ; 
+
     void Start()
     {
-        SetRandomCatImges();
+        SetRandomCatImages();
+        SetColorText();
+        IndicateRightAnswer();
+    }
+
+    void Update()
+    {
+        
+        
+        if(Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)){
+            if (rightAnswerDirection =="Up")
+            {
+                Console.WriteLine("HIIIIII");
+            }
+
+        }
+        if(Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S)){
+            if (rightAnswerDirection =="Down")
+            {
+                                Console.WriteLine("HIIIIII");
+
+            }
+        }
+        if(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)){
+            if (rightAnswerDirection =="Left")
+            {
+                Console.WriteLine("HIIIIII");
+
+            }
+        }
+        if(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)){
+            if (rightAnswerDirection =="Right")
+            {
+                Console.WriteLine("HIIIIII");
+
+            }
+            
+        }
+    }
+
+
+    
+
+
+    void SetColorText()
+    {
+        int randomNumber = GenerateRandomNumber(0, randomColors.Count);
+        rightAnswerColor = randomColors[randomNumber];
+        GameObject.Find("ColorText").GetComponent<TextMeshProUGUI>().text= rightAnswerColor.ToUpper() ;
     }
 
 
 
-    void SetRandomCatImges()
+
+    void SetRandomCatImages()
     {
-        List<string> cats = new List<string> { "black-cat","blue-cat","brown-cat","gray-cat","green-cat","orange-cat","pink-cat","purple-cat","red-cat","yellow-cat" };
-        List<string> randomCats = new List<string>();
-        System.Random random = new System.Random();
-        for (int i = 1; i <= 4; i++)
+        while (randomColors.Count < 4)
         {
-            int randomNumber = random.Next(1, cats.Count + 1);
-            randomCats.Add(cats[randomNumber]);
+           int randomNumber = GenerateRandomNumber(0, colors.Count);
+           if(!randomColors.Contains(colors[randomNumber]))
+                randomColors.Add(colors[randomNumber]);
 
         }
-        Sprite firstCat = Resources.Load<Sprite>("Sprites/"+randomCats[0]);
-        Sprite secondCat = Resources.Load<Sprite>("Sprites/"+randomCats[1]);
-        Sprite thirdCat = Resources.Load<Sprite>("Sprites/"+randomCats[2]);
-        Sprite fourthCat = Resources.Load<Sprite>("Sprites/"+randomCats[3]);
+        Sprite firstCat = Resources.Load<Sprite>("Sprites/"+randomColors[0]+"-cat");
+        Sprite secondCat = Resources.Load<Sprite>("Sprites/"+randomColors[1]+"-cat");
+        Sprite thirdCat = Resources.Load<Sprite>("Sprites/"+randomColors[2]+"-cat");
+        Sprite fourthCat = Resources.Load<Sprite>("Sprites/"+randomColors[3]+"-cat");
 
         GameObject.Find("CatUp").GetComponent<Image>().sprite = firstCat;
         GameObject.Find("CatDown").GetComponent<Image>().sprite = secondCat;
         GameObject.Find("CatRight").GetComponent<Image>().sprite = thirdCat;
         GameObject.Find("CatLeft").GetComponent<Image>().sprite = fourthCat;
 
-
+        directionWithCats.Add("Up", randomColors[0]);
+        directionWithCats.Add("Down", randomColors[1]);
+        directionWithCats.Add("Right", randomColors[2]);
+        directionWithCats.Add("Left", randomColors[3]);
 
 
     }
 
+    void IndicateRightAnswer(){
+        foreach (DictionaryEntry enrty in directionWithCats){
+            if(enrty.Value.Equals(rightAnswerColor)){
+                rightAnswerDirection = enrty.Key.ToString() ; 
+            }
+
+        }
+    }
+
+    int GenerateRandomNumber(int firstNumber, int lastNumber)
+    {
+        System.Random  random = new System.Random ();
+        return random.Next(firstNumber, lastNumber);
+    }
   
 }
