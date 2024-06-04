26a94d6de438d84bf40842d09b6ad25c0a6f8ece
nick-gharizadeh <nick-gharizadeh@gmail.com>
2024-06-05 00:14:05 +0330

    fea / play sounds in scenes

--- Assets/Scripts/RainbowCatAdventure.cs
+++ Assets/Scripts/RainbowCatAdventure.cs
@@ -2,67 +2,107 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class RainbowCatAdventure : MonoBehaviour
 {
-    List<string> colors= new List<string> { "black","blue","brown","gray","green","orange","pink","purple","red","yellow" };
+    List<string> colors = new List<string> { "black", "blue", "brown", "gray", "green", "orange", "pink", "purple", "red", "yellow" };
     List<string> randomColors = new List<string>();
+    int score = 0;
     Hashtable directionWithCats = new Hashtable();
-    string rightAnswerColor , rightAnswerDirection ; 
+    string rightAnswerColor, rightAnswerDirection;
 
     void Start()
     {
+        InitializeGame();
+    }
+
+    void Update()
+    {
+
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+        {
+            if (rightAnswerDirection == "Up")
+            {
+                PlayAudioSource("cute-meow");
+                UpdateScore();
+                InitializeGame();
+            }
+            else
+            {
+                PlayAudioSource("angry-meow");
+            }
+
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
+        {
+            if (rightAnswerDirection == "Down")
+            {
+                PlayAudioSource("cute-meow");
+                UpdateScore();
+                InitializeGame();
+            }
+            else
+            {
+                PlayAudioSource("angry-meow");
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        {
+            if (rightAnswerDirection == "Left")
+            {
+                PlayAudioSource("cute-meow");
+                UpdateScore();
+                InitializeGame();
+
+            }
+            else
+            {
+                PlayAudioSource("angry-meow");
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        {
+            if (rightAnswerDirection == "Right")
+            {
+                PlayAudioSource("cute-meow");
+                UpdateScore();
+                InitializeGame();
+            }
+            else
+            {
+                PlayAudioSource("angry-meow");
+            }
+
+
+        }
+    }
+
+
+    void InitializeGame()
+    {
+        randomColors.Clear();
+        directionWithCats.Clear();
         SetRandomCatImages();
         SetColorText();
         IndicateRightAnswer();
     }
 
-    void Update()
+    void UpdateScore()
     {
-        
-        
-        if(Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)){
-            if (rightAnswerDirection =="Up")
-            {
-                Console.WriteLine("HIIIIII");
-            }
-
-        }
-        if(Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S)){
-            if (rightAnswerDirection =="Down")
-            {
-                                Console.WriteLine("HIIIIII");
-
-            }
-        }
-        if(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)){
-            if (rightAnswerDirection =="Left")
-            {
-                Console.WriteLine("HIIIIII");
-
-            }
-        }
-        if(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)){
-            if (rightAnswerDirection =="Right")
-            {
-                Console.WriteLine("HIIIIII");
-
-            }
-            
-        }
+        score += 5;
+        GameObject.Find("ScoreText").GetComponent<TextMeshProUGUI>().text = score.ToString();
     }
-
-
-    
 
 
     void SetColorText()
     {
         int randomNumber = GenerateRandomNumber(0, randomColors.Count);
         rightAnswerColor = randomColors[randomNumber];
-        GameObject.Find("ColorText").GetComponent<TextMeshProUGUI>().text= rightAnswerColor.ToUpper() ;
+        GameObject.Find("ColorText").GetComponent<TextMeshProUGUI>().text = rightAnswerColor.ToUpper();
     }
 
 
@@ -72,15 +112,15 @@ public class RainbowCatAdventure : MonoBehaviour
     {
         while (randomColors.Count < 4)
         {
-           int randomNumber = GenerateRandomNumber(0, colors.Count);
-           if(!randomColors.Contains(colors[randomNumber]))
+            int randomNumber = GenerateRandomNumber(0, colors.Count);
+            if (!randomColors.Contains(colors[randomNumber]))
                 randomColors.Add(colors[randomNumber]);
 
         }
-        Sprite firstCat = Resources.Load<Sprite>("Sprites/"+randomColors[0]+"-cat");
-        Sprite secondCat = Resources.Load<Sprite>("Sprites/"+randomColors[1]+"-cat");
-        Sprite thirdCat = Resources.Load<Sprite>("Sprites/"+randomColors[2]+"-cat");
-        Sprite fourthCat = Resources.Load<Sprite>("Sprites/"+randomColors[3]+"-cat");
+        Sprite firstCat = Resources.Load<Sprite>("Sprites/" + randomColors[0] + "-cat");
+        Sprite secondCat = Resources.Load<Sprite>("Sprites/" + randomColors[1] + "-cat");
+        Sprite thirdCat = Resources.Load<Sprite>("Sprites/" + randomColors[2] + "-cat");
+        Sprite fourthCat = Resources.Load<Sprite>("Sprites/" + randomColors[3] + "-cat");
 
         GameObject.Find("CatUp").GetComponent<Image>().sprite = firstCat;
         GameObject.Find("CatDown").GetComponent<Image>().sprite = secondCat;
@@ -95,19 +135,30 @@ public class RainbowCatAdventure : MonoBehaviour
 
     }
 
-    void IndicateRightAnswer(){
-        foreach (DictionaryEntry enrty in directionWithCats){
-            if(enrty.Value.Equals(rightAnswerColor)){
-                rightAnswerDirection = enrty.Key.ToString() ; 
+    void IndicateRightAnswer()
+    {
+        foreach (DictionaryEntry enrty in directionWithCats)
+        {
+            if (enrty.Value.Equals(rightAnswerColor))
+            {
+                rightAnswerDirection = enrty.Key.ToString();
             }
 
         }
     }
 
+    void PlayAudioSource(string audioClipName)
+    {
+        AudioSource audioSource = GameObject.Find("AudioSource").GetComponent<AudioSource>();
+        audioSource.clip = Resources.Load<AudioClip>("Sounds/" + audioClipName);
+        audioSource.Play();
+
+    }
+
     int GenerateRandomNumber(int firstNumber, int lastNumber)
     {
-        System.Random  random = new System.Random ();
+        System.Random random = new System.Random();
         return random.Next(firstNumber, lastNumber);
     }
-  
+
 }
