282f85b2c789197195230cda6ea6543e9dd2df0b
nick-gharizadeh <nick-gharizadeh@gmail.com>
2024-06-18 19:33:19 +0330

    fea / make scripts clickable and add timer icon

--- Assets/Scripts/RainbowCatAdventure.cs
+++ Assets/Scripts/RainbowCatAdventure.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
@@ -21,98 +20,67 @@ public class RainbowCatAdventure : MonoBehaviour
     void Start()
     {
         InitializeGame();
+        SetOnClickListenerForImages();
     }
 
     void Update()
     {
 
-
         if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
         {
-            if (rightAnswerDirection == "Up")
-            {
-                PlayAudioSource("cute-meow");
-                UpdateScore();
-                InitializeGame();
-            }
-            else
-            {
-                PlayAudioSource("angry-meow");
-                lives--;
-            }
+            CheckUserAwnser("Up");
 
         }
         if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
         {
-            if (rightAnswerDirection == "Down")
-            {
-                PlayAudioSource("cute-meow");
-                UpdateScore();
-                InitializeGame();
-            }
-            else
-            {
-                PlayAudioSource("angry-meow");
-                lives--;
-            }
+            CheckUserAwnser("Down");
+
         }
         if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
         {
-            if (rightAnswerDirection == "Left")
-            {
-                PlayAudioSource("cute-meow");
-                UpdateScore();
-                InitializeGame();
-
-            }
-            else
-            {
-                PlayAudioSource("angry-meow");
-                lives--;
-            }
+            CheckUserAwnser("Left");
         }
         if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
         {
-            if (rightAnswerDirection == "Right")
-            {
-                PlayAudioSource("cute-meow");
-                UpdateScore();
-                InitializeGame();
-            }
-            else
-            {
-                PlayAudioSource("angry-meow");
-                lives--;
-            }
-
-
+            CheckUserAwnser("Right");
         }
-
     }
+
+
 
 
     void FixedUpdate()
     {
         timer -= Time.deltaTime;
+        GameObject.Find("TimeText").GetComponent<TextMeshProUGUI>().text = timer.ToString("0.00");
+        if (timer <= 2f && timer > 1.95f )
+        {
+            GameObject.Find("ImageClock").GetComponent<Image>().enabled = true;
+            GameObject.Find("TimeText").GetComponent<TextMeshProUGUI>().enabled = true;
+            PlayAudioSource("tictoc");
+        }
         if (timer <= 0f)
         {
             lives--;
             InitializeGame();
+            GameObject.Find("ImageClock").GetComponent<Image>().enabled = false;
+            GameObject.Find("TimeText").GetComponent<TextMeshProUGUI>().enabled = false;
+
         }
-                switch (lives)
+        switch (lives)
         {
             case 0:
-            GameObject.Find("HeartImage1").GetComponent<Image>().enabled = false;
-            PlayerPrefs.SetInt("FinalScore", score);
-            SceneManager.LoadScene("GameOverScene");
+                GameObject.Find("HeartImage1").GetComponent<Image>().enabled = false;
+                PlayerPrefs.SetInt("FinalScore", score);
+                SceneManager.LoadScene("GameOverScene");
                 break;
 
             case 1:
-            GameObject.Find("HeartImage2").GetComponent<Image>().enabled = false;
+                GameObject.Find("HeartImage2").GetComponent<Image>().enabled = false;
                 break;
 
             case 2:
-            GameObject.Find("HeartImage3").GetComponent<Image>().enabled = false;
+                GameObject.Find("HeartImage3").GetComponent<Image>().enabled = false;
                 break;
 
         }
@@ -127,6 +95,31 @@ public class RainbowCatAdventure : MonoBehaviour
         SetRandomCatImages();
         SetColorText();
         IndicateRightAnswer();
+    }
+
+    void CheckUserAwnser(string userAwnser)
+    {
+        if (rightAnswerDirection == userAwnser)
+        {
+            PlayAudioSource("cute-meow");
+            UpdateScore();
+            InitializeGame();
+        }
+        else
+        {
+            PlayAudioSource("angry-meow");
+            lives--;
+        }
+
+    }
+
+    void SetOnClickListenerForImages()
+    {
+        GameObject.Find("CatUp").GetComponent<Button>().onClick.AddListener(() => CheckUserAwnser("Up"));
+        GameObject.Find("CatDown").GetComponent<Button>().onClick.AddListener(() => CheckUserAwnser("Down"));
+        GameObject.Find("CatRight").GetComponent<Button>().onClick.AddListener(() => CheckUserAwnser("Right"));
+        GameObject.Find("CatLeft").GetComponent<Button>().onClick.AddListener(() => CheckUserAwnser("Left"));
+
     }
 
     void UpdateScore()
